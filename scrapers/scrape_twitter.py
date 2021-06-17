297c9d0d75a5ca21ed1a001f250dcdaf68045a70
suuuudo <sudonyym@gmail.com>
2021-06-16 22:58:20 -0700

    Added twitter scraping initialization

--- scrapers/scrape_twitter.py
+++ scrapers/scrape_twitter.py
@@ -1,4 +1,12 @@
 import os
-tweets = os.system(
-    "snscrape --jsonl --max-results 100 twitter-search 'from:elonmusk'")
-print(tweets)
+import json
+import subprocess
+query = "snscrape --jsonl --max-results 100 twitter-search 'from:elonmusk'"
+# os.system()
+tweets = subprocess.check_output(query, shell=True).decode('utf-8')
+# print(tweets)
+tweet_list = tweets.split('}\n')
+
+tweet_list = list(map(lambda s: s + '}', tweet_list))
+print(tweet_list[0])
+json_tweets = json.loads(tweets)
