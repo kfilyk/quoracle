00e8a3d3f82380dd143339f7a2187e0d1c346550
kfilyk <kelvinfilyk@gmail.com>
2021-07-11 16:13:30 -0700

    Added tweet replies, quotes, json

--- scrapers/TwitterScraper.py
+++ scrapers/TwitterScraper.py
@@ -5,6 +5,15 @@ from Scraper import Scraper
 from langdetect import detect
 from datetime import datetime, timedelta
 from pathlib import Path
+from emoji import UNICODE_EMOJI
+
+
+def is_emoji(s):
+    return s in UNICODE_EMOJI
+
+
+def add_space(text):
+    return ''.join(' ' + char if is_emoji(char) else char for char in text).strip()
 
 
 class TwitterScraper(Scraper):
@@ -19,7 +28,7 @@ class TwitterScraper(Scraper):
         # self.query = "snscrape --jsonl --max-results 100 --since 2020-06-01 twitter-search 'from:elonmusk until:2020-06-02'"
         # self.query = "snscrape --jsonl --max-results 100 --since 2020-06-01 twitter-search '#cocacola until:2020-06-02'"
         # --max-results 100
-        #query = "snscrape --jsonl --max-results 1000 --since 2020-06-01 twitter-search 'TSLA until:2020-06-02'"
+        # query = "snscrape --jsonl --max-results 1000 --since 2020-06-01 twitter-search 'TSLA until:2020-06-02'"
         # self.query = "snscrape --jsonl twitter-search 'from:JCOviedo6'"
         # self.query = "snscrape --jsonl --max-results 1000 twitter-search '1267031143636926465'"
 
@@ -56,150 +65,111 @@ class TwitterScraper(Scraper):
         return extracted_json
 
     def scrape(self, date, stock):
-        threads = {}  # start building a dictionary of concatenated conversation objects, created from amalgamation of tweets
+        conversation_ids = []
+        conversations = {}  # start building a dictionary of concatenated conversation objects, created from amalgamation of tweets
         date_start = date.strftime('%Y-%m-%d')
         date_end = (date+timedelta(days=1)).strftime('%Y-%m-%d')
 
         extracted_json = self.call_snscrape(
-            "snscrape --jsonl --max-results 1000 --since %s twitter-search '%s until:%s'" % (date_start, stock, date_end))
+            "snscrape --jsonl --max-results 20 --since %s twitter-search '%s until:%s'" % (date_start, stock, date_end))
 
-        i = 1
-        for t in extracted_json:  # for every tweet
+        for e in extracted_json:  # for every tweet
+            if e['lang'] != 'en':  # if the main language isn't english, ignore this conversation. Not worth it to parse in vader
+                extracted_json.remove(e)
+                continue
+            conversation = {}
+            # print(e)
+            conversation_id = e['conversationId']
+            if conversation_id in conversation_ids:  # if this conversation has already been recorded, don't add a duplicate
+                print("CONVERSATION ALREADY ADDED. SKIPPING...\n")
+                continue
 
-            # if('mentionedUsers' in t) and (t['mentionedUsers'] != None):
-            # print("%s\n" % t)  # print each tweet
-            thread = {}
-            # each thread has a convo id I assume???
-            content = t['content']
-            mentioned = []
-            links = []
-            cashtags = []
-            hashtags = []
-            if t['tcooutlinks'] != None:
-                links = t['tcooutlinks']
-            if t['cashtags'] != None:
-                cashtags = t['cashtags']
-            if t['hashtags'] != None:
-                hashtags = t['hashtags']
-            if ('mentionedUsers' in t) and (t['mentionedUsers'] != None):
-                for m in t['mentionedUsers']:
-                    mentioned.append(m['username'])
+            conversation_ids.append(conversation_id)
+            print('ADDING CONVERSATION %d FROM %s...\n' %
+                  (conversation_id, e['url']))
 
-            q_ids = []
-            q = t['quotedTweet']  # get chain of all previously quoted tweets
-            while q is not None:  # get chain of all
-                if q['id'] not in q_ids:  # continue as long as there isnt a loop
-                    content = q['content'] + '\n' + content
-                    if q['tcooutlinks'] is not None:
-                        links.extend(q['tcooutlinks'])  # add to set
-                    if q['cashtags'] is not None:
-                        cashtags.extend(q['cashtags'])
-                    if q['hashtags'] is not None:
-                        hashtags.extend(q['hashtags'])
-                    if ('mentionedUsers' in q) and (q['mentionedUsers'] != None):
-                        for m in q['mentionedUsers']:
-                            mentioned.append(m['username'])
-                    q_ids.append(q['id'])
-                    q = q['quotedTweet']
-                else:
-                    break
-            """
-            r_ids = []
-            r = t['inReplyToTweetId']
-            while r is not None:
-                if r not in r_ids:
-                    # if this tweet was a comment, find and append the original tweet
-                    ptid = t['inReplyToTweetId']  # parent tweet id
-                    print("TWEET ID:" + ptid)
-                    print("CONVO ID: " + t['conversationId'])
-                    print(t)
-                    parent_tweet = self.call_snscrape(
-                        "snscrape --jsonl twitter-search 'from:%s since_id:%d max_id:%d filter:safe'" % (t['inReplyToUser']['username'], ptid-1, ptid+1))
-                    if len(parent_tweet) == 0:
-                        from_date = '2006-03-22'
-                        if t['inReplyToUser']['created'] is not None:
-                            from_date = datetime.strptime(
-                                t['inReplyToUser']['created'][:10], '%Y-%m-%d')
-                        until_date = datetime.strptime(
-                            t['date'][:10], '%Y-%m-%d')
-                        print(from_date)
-                        print(until_date)
-                        until_date += datetime.timedelta(days=1)
-                        # get list of all tweets from the conversation parent profile
-                        parent_tweet = self.call_snscrape(
-                            "snscrape --jsonl twitter-search --since %s 'from:%s until:%s'" % (from_date, t['inReplyToUser']['username'], until_date))
-                        for pt in parent_tweet:
-                            # if this is actually the conversation
-                            if pt['conversationId'] != t['conversationId']:
-                                if pt['id'] != t['inReplyToTweetId']:
-                                    parent_tweet.remove(pt)
-                        print(len(parent_tweet))
-                    if parent_tweet['content'] is not None:
-                        content = parent_tweet['content'] + '\n' + content
-                    if parent_tweet['tcooutlinks'] is not None:
-                        links.extend(parent_tweet['tcooutlinks'])  # add to set
-                    if parent_tweet['cashtags'] is not None:
-                        cashtags.extend(parent_tweet['cashtags'])
-                    if parent_tweet['hashtags'] is not None:
-                        hashtags.extend(parent_tweet['hashtags'])
-                    if ('mentionedUsers' in parent_tweet) and (parent_tweet['mentionedUsers'] != None):
-                        for m in parent_tweet['mentionedUsers']:
-                            mentioned.append(m['username'])
+            conversation_json = self.call_snscrape(  # conversation_json is ordered most recent last
+                "snscrape --jsonl twitter-search 'conversation_id:%d filter:safe'" % (conversation_id))
 
-                    r_ids.append(r['id'])
-                    print(parent_tweet)
+            quoted_json = self.call_snscrape(
+                "snscrape --jsonl twitter-search 'quoted_tweet_id:%d filter:safe'" % (conversation_id))
 
-                    r = r['parentTweet']
-                else:
-                    break
-                """
+            for q in quoted_json:
+                conversation_json.append(q)
 
-            links = set(links)
-            cashtags = set(cashtags)
-            hashtags = set(hashtags)
-            mentioned = set(mentioned)
-            if cashtags is not None:
-                for c in cashtags:
-                    content = content.replace('$'+c, c)
-            if hashtags is not None:
-                for h in hashtags:
-                    content = content.replace('#'+h, h)
-            if links is not None:
-                for l in links:
-                    content = content.replace(l, '')
-            if mentioned is not None:
-                for m in mentioned:
-                    content = content.replace('@'+m, '')
-            content = content.replace('\n', ' ')
-            redundant_links = [
-                t for t in content.split() if t.startswith('https://t.co/')]
-            if redundant_links is not None:
-                for l in redundant_links:
-                    content = content.replace(l, '')
+            parent_json = []  # note: Sometimes parent tweets just dont exist???
+            if e['inReplyToUser'] == None:
+                parent_json.append(e)
+            else:
+                parent_json = self.call_snscrape(
+                    "snscrape --jsonl twitter-search 'since_id:%d max_id:%d filter:safe'" % (conversation_id-1, conversation_id))
 
-            print("%d. %s | LIKES: %s | FOLLOWERS: %s | CASHTAGS: %s | HASHTAGS: %s | MENTIONED: %s | ORIGINAL: %s | LINKS: %s | \n %s\n" % (
-                i, t['user']['username'], t['likeCount'], t['user']['followersCount'], cashtags, hashtags, mentioned, t['url'], links, content))
+            if len(parent_json) != 0:
+                conversation_json.append(parent_json[0])
 
-            thread['number'] = i
-            thread['username'] = t['user']['username']
-            thread['followers'] = t['user']['followersCount']
-            thread['likes'] = t['likeCount']
-            thread['url'] = t['url']
-            thread['links'] = list(links)
-            thread['mentioned'] = list(mentioned)
-            thread['cashtags'] = list(cashtags)
-            thread['hashtags'] = list(hashtags)
-            thread['content'] = content
-            threads[t['conversationId']] = thread
-            i = i+1
+            # append quoted tweet if parent is retweeting an old tweet
+            if conversation_json[len(conversation_json)-1]['quotedTweet'] != None:
+                conversation_json.append(
+                    conversation_json[len(conversation_json)-1]['quotedTweet'])
 
-        print("(%d TWEETS)" % (i-1))
+            for tweet in conversation_json:  # for each tweet in conversation
+                # space out emojis, remove new lines
+                reply = {}
+                content = add_space(tweet['content'])
+                content = content.replace('\n', ' ')
+                mentioned = []
+                links = []
+                cashtags = []
+                hashtags = []
+                if tweet['tcooutlinks'] != None:
+                    links = tweet['tcooutlinks']
+                if tweet['cashtags'] != None:
+                    cashtags = tweet['cashtags']
+                if tweet['hashtags'] != None:
+                    hashtags = tweet['hashtags']
+                if ('mentionedUsers' in tweet) and (tweet['mentionedUsers'] != None):
+                    for m in tweet['mentionedUsers']:
+                        mentioned.append(m['username'])
+
+                links = set(links)
+                cashtags = set(cashtags)
+                hashtags = set(hashtags)
+                mentioned = set(mentioned)
+
+                redundant_links = [
+                    l for l in content.split() if l.startswith('https://t.co/')]
+                if redundant_links is not None:
+                    for l in redundant_links:
+                        content = content.replace(l, '')
+
+                # print("%s: @%s | LIKES: %s | FOLLOWERS: %s | CASHTAGS: %s | HASHTAGS: %s | MENTIONED: %s | ORIGINAL: %s | LINKS: %s | \n %s\n" % (
+                    # tweet['date'], tweet['user']['username'], tweet['likeCount'], tweet['user']['followersCount'], cashtags, hashtags, mentioned, tweet['url'], links, content))
+
+                reply['username'] = tweet['user']['username']
+                reply['followers'] = tweet['user']['followersCount']
+                reply['likes'] = tweet['likeCount']
+                reply['links'] = list(links)
+                reply['mentioned'] = list(mentioned)
+                reply['cashtags'] = list(cashtags)
+                reply['hashtags'] = list(hashtags)
+                reply['content'] = content
+                conversation[tweet['date']] = reply
+            if len(parent_json) != 0:
+                conversation['url'] = parent_json[0]['url']
+            else:
+                conversation['url'] = conversation_json[len(
+                    conversation_json)-1]['url']
+
+            conversations[conversation_id] = conversation
+
+        print("%d tweets analyzed, %d conversations" %
+              (len(extracted_json), len(conversation_ids)))
         file_path = '../scraped_data/%s/twitter.json' % (date_start)
         Path("../scraped_data/%s/" % (date_start)
              ).mkdir(parents=True, exist_ok=True)
         with open(file_path, 'w+') as f:
-            json.dump(threads, f)
-            #threads = json.dumps(threads)
+            json.dump(conversations, f)
+            # threads = json.dumps(threads)
 
 
 ts = TwitterScraper()
